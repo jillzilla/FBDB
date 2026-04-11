76cbb00b65d62f54e7a4f6e1caf0780d69f68bf5
Jill Zilla <jillzilla11037@gmail.com>
2026-04-11 20:51:50 +0200

    you can now skip in game over scene

--- assets/scripts/game_over.gd
+++ assets/scripts/game_over.gd
@@ -6,3 +6,6 @@ func _ready() -> void:
 
 func _on_timer_timeout() -> void:
 	get_tree().change_scene_to_file("res://assets/scenes/menu.tscn");
+
+func _on_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://assets/scenes/menu.tscn");
