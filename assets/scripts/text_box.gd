897a6a5afde3c27a43e95353a6239144185c8c42
Jill Zilla <jillzilla11037@gmail.com>
2026-04-12 23:24:26 +0200

    assets and dialogues (wip)

---
+++ assets/scripts/text_box.gd
@@ -0,0 +1,74 @@
+extends CanvasLayer
+
+signal text_end;
+
+#variables
+const CHAR_READ_RATE : float = 0.03;
+
+@onready var tween : Tween = null;
+@export var text_box_container : MarginContainer;
+@export var text_dialogue : Label;
+
+enum States
+{
+	READY,
+	READING,
+	FINISHED,
+}
+
+var current_state : Variant = States.READY;
+var text_queue : Array[String] = [];
+
+#functions
+func _ready() -> void:
+	text_end.connect(_text_ended);
+	_hide_textbox();
+
+func _hide_textbox() -> void:
+	text_box_container.visible = false;
+	text_dialogue.text = "";
+
+func _process(_delta: float) -> void:
+	match(current_state):
+		States.READY:
+			if !text_queue.is_empty():
+				_display_text();
+		States.READING:
+			if Input.is_action_just_pressed("NextStage"):
+				text_dialogue.visible_ratio = 1;
+				tween.stop();
+				_change_state(States.FINISHED);
+		States.FINISHED:
+			if Input.is_action_just_pressed("NextStage"):
+				_change_state(States.READY);
+				_hide_textbox();
+
+func _show_textbox() -> void:
+	text_box_container.visible = true;
+
+func _queue_text(t: String) -> void:
+	text_queue.push_back(t);
+
+func _display_text() -> void:
+	var t : String = text_queue.pop_front();
+
+	if tween:
+		tween = null;
+	tween = create_tween();
+	
+	text_dialogue.text = t;
+	text_dialogue.visible_ratio = 0;
+	
+	_change_state(States.READING);
+	_show_textbox();
+	
+	tween.tween_property(text_dialogue,"visible_ratio", 1, len(t) * CHAR_READ_RATE);
+	await tween.finished;
+	
+	text_end.emit();
+
+func _text_ended() -> void:
+	_change_state(States.FINISHED);
+
+func _change_state(next_state : Variant) -> void:
+	current_state = next_state;
